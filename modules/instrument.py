8a2417c34ccfe814e695bcf1dc5fb3cbfec26648
Théo MARINHO <theo.marinho@epitech.eu>
2020-10-06 14:51:39 +0200

    fix app initialization

--- modules/instrument.py
+++ modules/instrument.py
@@ -3,19 +3,18 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 class Instrument:
     name = ""
     values = []
 
     def __init__(self, name, fx):
+        load_dotenv()
         self.name = name
         self.fx = fx
         self.init_clandles()
 
     def init_clandles(self):
-        d = self.fx.get_candles(self.name, period=og.getenv("UPDATE_PERIOD"), number=os.getenv("CANDLES_TO_GET_ON_INIT"))
+        d = self.fx.get_candles(self.name, period="m1", number=10)
         self.values.insert(0, d)
 
     def update(self):
