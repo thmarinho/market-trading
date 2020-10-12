567191dca9abd36a403fda7c03f9fc38342188b3
Théo MARINHO <theo.marinho@epitech.eu>
2020-10-12 16:37:10 +0200

    add config file instead of .env

--- app.py
+++ app.py
@@ -5,20 +5,24 @@ import sys
 import re
 import os
 from modules.instrument import Instrument
-from dotenv import load_dotenv
+from modules.color import colors
 
 _instruments = []
 
 def main():
-    load_dotenv()
-    fx = fxcmpy.fxcmpy(access_token=os.getenv("FXCM_API_KEY"), log_level="error")
+    fx = fxcmpy.fxcmpy(config_file="./config/fxcm.cfg")
     print("Connection to FXCM etablished")
     instruments = fx.get_instruments()
     for i in instruments:
         if re.compile("[A-Z]{3}\/[A-Z]{3}").match(i):
-            d = Instrument(i, fx)
-            _instruments.append(d)
-            print('Succesfully init class for {}'.format(i))
+            try:
+                d = Instrument(i, fx)
+                _instruments.append(d)
+                print("[" + colors.OK + " OK " + colors.DEF + "] Initializing class for {}".format(i))
+            except:
+                print("[" + colors.FAIL + " KO " + colors.DEF + "] Initializing class for {}".format(i))
+                fx.close()
+                sys.exit(1)
     fx.close()
 
 if __name__ == '__main__':
---
+++ modules/color.py
@@ -0,0 +1,5 @@
+class colors:
+    OK="\033[92m"
+    WARNING="\033[93maze"
+    FAIL="\033[1m\033[91m"
+    DEF="\033[0m"
